e39839cb9581e3c16435e1487542d15fee9ea13d
cc-Tanaka <tanaka-ryo@colorfulcompany.co.jp>
2020-12-21 11:06:26 +0900

    reset

--- src/App.vue
+++ src/App.vue
@@ -1,113 +1,21 @@
 <template>
   <div>
-    <h4>{{ $store.state.message }}</h4>
-    <!-- preventでpostする事を防ぐ -->
-    <form>
-      <input v-model="todo">
-      <button @click="submit" val="追加"/>
-    </form>
-    <ul v-for="item in list" :key="item">
-      <li v-on:click="doneTodo(item)">
-        <span>{{item.status}}</span>
-        <span v-if="item.status == 'done'">
-          {{item.text}}
-        </span>
-        <span v-else>
-          {{item.text}}
-        </span>
-      </li>
-    </ul>
-    <div>
-      <h4>Count</h4>
-      {{ count }}
-      <button @click="increment">+</button>
-      <button @click="decrement">-</button>
-    </div>
-    <div>
-      <h4>Ky_Universal</h4>
-      <div>
-        {{ this.result }}
-      </div>
-    </div>
-    <div>
-      <h4>POST</h4>
-      <button @click="post_request">POST</button>
-      <div v-if="post_result_status == 200">
-        <p>status: {{ post_result_message }}</p>
-      </div>
-      <div v-else>
-        <p>status: {{ post_result_status }}</p>
-      </div>
-    </div>
+    <h4>Hello World</h4>
   </div>
 </template>
 
 <script>
-// import Feacher from './lib/ky.js'
-// import { hoge, fuga } from './lib/test.js'
-import Feacher from './lib/ky.js'
-import Post from './lib/post.js'
-
 
 export default {
   data() {
     return {
-      todo: "",
-      list: [],
-      result: "",
-      post_result: "",
-      post_result_status: "",
-      post_result_message: "",
     }
   },
   methods: {
-    sendTodo: function () {
-      this.list.push({
-        text: this.todo,
-        status: 'todo'
-      })
-      this.todo = ""
-    },
-    doneTodo: function (item) {
-      if(item.status == 'done') {
-        item.status = 'todo'
-      } else {
-        item.status = 'done'
-      }
-    },
-    increment() {
-      this.$store.dispatch('increment')
-    },
-    decrement() {
-      this.$store.dispatch('decrement')
-    },
-    submit() {
-      this.sendTodo()
-    },
-    async post_request() {
-      try {
-        const post = new Post()
-        this.post_result = await post.post_test()
-        this.post_result_status = this.post_result["status"]
-        this.post_result_message = "200"
-      } catch(error) {
-        console.log(error.response["status"])
-        this.post_result_message = "400"
-      }
-    }
   },
   computed: {
-    count () {
-      return this.$store.state.count
-    }
   },
-  async mounted() {
-    const feacher = new Feacher()
-    try {
-      this.result = await feacher.feach()
-    } catch (error) {
-      this.result = error.message
-    }
+  mounted() {
   }
 }
 </script>
--- src/lib/test.js
+++
@@ -1,9 +0,0 @@
-import ky from 'ky-universal'
-
-export function hoge() {
-  return ky.get('https://cc-api-practice.herokuapp.com/api/v1/nwjbhdunck').json()
-}
-
-export function fuga() {
-  return Promise.resolve('fuga')
-}
